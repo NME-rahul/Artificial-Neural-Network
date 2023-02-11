fff173803d170f33fb633a5efdd03849e6cbef79
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2023-02-11 16:17:33 +0530

    Update FirstModel.cpp

--- Implementation/FirstModel.cpp
+++ Implementation/FirstModel.cpp
@@ -1,40 +1,45 @@
 /*
-y = w * x => sop
+y = wx => sop
+Error: have problem variable loosing their value after multiple function call
 */
 
-#include<bits/stdc++.h>
+
+#include<iostream>
+#include<math.h>
 
 class Neuron{
-  long x;
-  long real;
-  long learning rate;
-  long w;
-public:
-  long sop = w * x;
+  static float x;
+  static float real;
+  static float learning_rate;
+  static float w;
+  static float sop;
   
-  Neuron(long x, long real, long learning_rate, long w){
-     long x = x;
-     long real = real;
-     long learning rate = learning_rate;
-     long w = w;
+public:
+  Neuron(float x, float real, float learning_rate, float w){
+     x = x;
+     real = real;
+     learning_rate = learning_rate;
+     w = w;
+     sop = w * x;
    }
-  long error(long predicted){
-    return pow((predicted - real), 2);
+  float error(float *predicted){
+    return pow(((*predicted) - real), 2);
   }
-  long sigmoid(){
+  float sigmoid(){
     return 1.0 / ( 1 + exp(-1 * sop));
   }
-  long error_prediction_deriv(long predicted){
-    return 2 * (predicted - real);
+  float error_prediction_deriv(float *predicted){
+    return 2 * ((*predicted) - real);
   }
-  long prediction_sop_deriv(long sop){
-    return sigmoid(sop) * (1.0 - sigmoid(sop));
+  float prediction_sop_deriv(){
+    return sigmoid() * (1.0 - sigmoid());
   }
-  long sop_w_deriv(){
+  float sop_w_deriv(){
     return x;
   }
-  long update_weight(grad){
-    w = w - (learning_rate * grad);
+  void update_w(float *grad){
+    w = w - (learning_rate * (*grad));
+    sop = w * x;
   }
 };
 
@@ -44,26 +49,29 @@ int main()
   Neuron *n = new Neuron(2.0, 1.0, 0.01, 0.5);
     
   short int epochs;
-  cin >> epochs;
+  std::cout << "Enter epochs: ";
+  std::cin >> epochs;
   for(int i = 0; i < epochs; i++){
     //Forward pass
-    long *predicted = new long(n->sigmoid());
-    long *err = new long(n->error(predicted));
-    std::cout << "Prediction: " << predicted << ", Error: " << err << std::endl;
+    float *predicted = new float(n->sigmoid());
+    float *err = new float(n->error(predicted));
+    std::cout << "Prediction: " << *predicted << ", Error: " << *err << std::endl;
+      
     //Backward pass
-    long *g1 = new long(n->error_prediction_deriv(predicted));
-    long *g2 = new long(n->prediction_sop_deriv());
-    long *g3 = new long(n->sop_w_deriv());
-    
-    long *grad = new long(g1 * g2 * g3);
+    float *g1 = new float(n->error_prediction_deriv(predicted));
+    float *g2 = new float(n->prediction_sop_deriv());
+    float *g3 = new float(n->sop_w_deriv());
+
+    float *grad = new float((*g1) * (*g2) * (*g3));
     n->update_w(grad); //updating weight
+      
+    delete predicted;
+    delete err;
+    delete g1;
+    delete g2;
+    delete g3;
+    delete grad;
   }
-  
-  delete predicted;
-  delete err;
-  delete g1;
-  delete g2;
-  delete g3;
-  delete grad;
+    
   return 0;
 }

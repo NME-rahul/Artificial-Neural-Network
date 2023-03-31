64308e3e464eee84b63f47958d430adcdf746047
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2023-03-31 23:30:04 +0530

    Update nn.py

--- General/nn.py
+++ General/nn.py
@@ -10,4 +10,20 @@ def fit(model, input_data, epochs, num_class, learning_rate, target):
     return result
         
 def summary(model):
-   print('total paramenters: ', model[0])
+   cnt = 0; total = 0
+   
+   cnt = len(model[0]['weight'][:,0]) * len(model[0]['weight'][0])
+   print('Trainable parameters in input layer: %d \nNumber of inputs: %d' %(cnt , model[0]['info']['num_inputs']))
+   
+   total = cnt; cnt = 0
+   for i in range(1, len(model) - 1):
+       cnt = cnt + (len(model[i]['weight'][0]) * len(model[i]['weight'][:,0]))
+      
+   print('\nNumber of Hidden layer: %d \nTrainable parameters in hidden layer: %d \nedges: %s ' %(len(model) - 2, cnt, model[1]['info']['edges']) )
+   
+   total = total + cnt; cnt = 0
+   cnt = model[-1]['info']['num_class']
+   print('\nNumber of class: %d \nedges: %s' %(cnt , model[-1]['info']['edges']))
+   total = total + cnt
+   
+   print('\nTotal trainaable prameters: ',total)

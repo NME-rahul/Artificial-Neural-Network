6ffc8aa751f1462fd921d084cf05583ff15ced37
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2023-04-01 00:13:07 +0530

    Update layers.py

--- General/layers.py
+++ General/layers.py
@@ -1,32 +1,23 @@
 import numpy as np
 
-def Input(num_inputs, activationFun, **features):
+def Input(num_inputs, activationFun, name = 'Input_layer', **features):
     weight = np.array([i for i in np.random.rand(num_inputs * num_inputs)]).reshape(num_inputs, num_inputs)
-    if 'name' not in features:
-        features['name'] = 'Input'
-    return {'weight': weight, 'info': {'num_inputs': num_inputs, 'activationFun': activationFun, 'name': features['name']}}
+    return {'weight': weight, 'info': {'num_inputs': num_inputs, 'activationFun': activationFun, 'name': name}}
         
         
-def Hidden(num_neurons, activationFun, **features):
+def Hidden(num_neurons, activationFun, edges = 'fully_connected', name = 'Hidden_layer', **features):
     weight = np.array([i for i in np.random.rand(num_neurons * num_neurons)]).reshape(num_neurons, num_neurons)
-    if 'name' not in features:
-        features['name'] = 'Input'
-    elif 'edges' not in features:
-        features['edges'] = 'fully_connected'
-    if features['edges'] == 'fully_connected' or features['edges'] == None:
-        return {'weight': weight, 'info': {'num_neurons': num_neurons, 'edges': features['edges'], 'activationFun': activationFun, 'name': features['name']}}
-    if features['edges'] == 'partial':
-        return {'weight': weight, 'info': {'num_neurons': num_neurons, 'edges': features['edges'], 'activationFun': activationFun, 'name': features['name']}}
+    if edges == 'fully_connected' or edges == None:
+        return {'weight': weight, 'info': {'num_neurons': num_neurons, 'edges': edges, 'activationFun': activationFun, 'name': name}}
+    if edges == 'partial':
+        return {'weight': weight, 'info': {'num_neurons': num_neurons, 'edges': edge, 'activationFun': activationFun, 'name': name}}
     
     
-def Output(num_neurons, activationFun, **features):
+def Output(num_neurons, activationFun, edges = 'fully_connected', name = 'Output_layer', **features):
     weight = np.array([i for i in np.random.rand(num_neurons)]).reshape(num_neurons, 1)
-    if 'name' not in features:
-        features['name'] = 'Input'
-    elif 'edges' not in features:
-        features['edges'] = 'fully_connected'
-    if features['edges'] == 'fully_connected' or features['edges'] == None:
-        return {'weight': weight, 'info': {'num_class': num_neurons, 'edges': features['edges'], 'activationFun': activationFun, 'name': features['name']}}
-    if features['edges'] == 'partial':
-        return {'weight': weight, 'info': {'num_class': num_neurons, 'edges': features['edges'], 'activationFun': activationFun, 'name': features['name']}}
+    if edges == 'fully_connected' or edges == None:
+        return {'weight': weight, 'info': {'num_class': num_neurons, 'edges': edges, 'activationFun': activationFun, 'name': name}}
+    if edges == 'partial':
+        return {'weight': weight, 'info': {'num_class': num_neurons, 'edges': edge, 'activationFun': activationFun, 'name': name}}
+    
     

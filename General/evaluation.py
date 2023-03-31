3c476253f6ef90f30db93c15a027369ca5ba1a08
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2023-04-01 00:12:33 +0530

    Update evaluation.py

--- General/evaluation.py
+++ General/evaluation.py
@@ -1,44 +1,44 @@
 import numpy as np
-import activationFunctions
-
-def sopOut(sop, activationFun):
-    return 
+import activationFunctions as af
 
 def squaredError(prediction, target):
     return np.power((prediction - target), 2)
     
-def forwardPropogation(model, data, epochs, num_class, target):
-    result = np.array([])
+def forwardPropgation(model, data, epochs, num_class, target):
+    prediction = []; result = np.array([]); 
+    num_inputs = model[0]['info']['num_inputs']
     num_neurons = model[1]['info']['num_neurons']
     sops = []; temp = 0
-    for i in range(epochs):
-        
+    for p in range(epochs):
         #for input layer
-        for i in range(num_neurons):
+        for i in range(num_inputs):
             for j in range(num_neurons):
                 temp = temp + (data[i][0] * model[0]['weight'][j][i])
-            temp = activation(sop, activationFun =  model[0]['info']['activationFun'])
+            temp = af.activation(temp, activationFun =  model[0]['info']['activationFun'])
             sops.append( temp )
             
         #for hidden layers
-        for k in range(len(model - 2)):
+        for k in range(1, len(model) - 1):
             for i in range(num_neurons):
                 for j in range(num_neurons):
-                    temp = sops[i] * model[k + 1]['weight'][j][i]
-                temp = activation(sop, activationFun = model[1]['info']['activationFun'])
+                    temp = sops[i] * model[k]['weight'][j][i]
+                temp = af.activation(temp, activationFun = model[1]['info']['activationFun'])
                 sops.append( temp )
             
         #for output layer
         for i in range(num_class):
             for j in range(num_neurons):
-                temp = temp + (sops[j*-1 - 1] * model[-1]['weight'][j*-1 - 1][i*-1 -1])
+                temp = temp + (sops[j*-1 - 1] * model[-1]['weight'][j*-1 - 1][i*-1 - 1])
             sops.append( temp ) #it appends the last nrurons output first
         
         for i in range(num_class):
-            prediction = activation(sop[i*-1 -1], activationFun =  model[-1]['info']['activationFun']) #now we again reverse the order(of output layer) of vlaues so it come again in its original order.
-            np.append(result, prediction, squaredError(prediction[i], target[i]))
+            prediction.append(af.activation(sops[i*-1 - 1], activationFun =  model[-1]['info']['activationFun'])) #now we again reverse the order(of output layer) of vlaues so it come again in its original order.
+            result = np.append(result, prediction[i])
+            result = np.append(result, squaredError(prediction[i], target[i]))
             
-        result.reshape(num_class, 2)
+    result.reshape(epochs, 2)
+            
+    print(result)
                 
     return result
     
@@ -47,5 +47,3 @@ def update_w(weight, learning_rate, grad):
     
 def backwardPropogation(model, epochs, num_class, learning_rate):
     return 
-    
-    

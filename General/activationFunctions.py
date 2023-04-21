c03d7bfcafd4f024a8e6d4e9d4fa1c79d2d8229a
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2023-04-21 12:11:14 +0530

    Update activationFunctions.py

--- General/activationFunctions.py
+++ General/activationFunctions.py
@@ -33,3 +33,38 @@ def activation(sop, activationFun):
     
     if activationFun == 'tanh':
         return
+
+
+def sop_activation_deriv(sop, activationFun):
+    if activationFun == 'sigmoid':
+        return activation(sop, activationFun) * (1 - activation(sop, activationFun))
+    
+    if  activationFun == 'softmax':
+        return 
+    
+    if activationFun == 'relu':
+        return
+    
+    if activationFun == 'relu6':
+        return
+    
+    if activationFun == 'leaky_relu':
+        return
+    
+    if activationFun == 'perm_relu':
+        return
+    
+    if activationFun == 'binary_step':
+        return
+    
+    if activationFun == 'iden':
+        return
+    
+    if activationFun == 'swish':
+        return
+    
+    if activationFun == 'hard_swish':
+        return
+    
+    if activationFun == 'tanh':
+        return
